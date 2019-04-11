20b6d016f69e813bd9720a3f5e0d3f3e484ed65c
Stefano <48553214+vailatis@users.noreply.github.com>
2019-04-11 14:07:05 +0200

    Add files via upload

--- web_interface/_library/_config.php
+++ web_interface/_library/_config.php
@@ -2,13 +2,16 @@
 //---------------------------------------------------------------------------------------------
 // MYSQL credentials
 $DB_Hostname = "127.0.0.1";
-$DB_Username = "solar";
-$DB_Password = "database password";
-$DB_Schema   = "solar_db";
+$DB_Username = "database username here";
+$DB_Password = "database password here";
+$DB_Schema   = "database schema name here";
 //---------------------------------------------------------------------------------------------
 // Configure the default TimeZone for Date/Time functions
 date_default_timezone_set("Europe/Rome");
 //---------------------------------------------------------------------------------------------
+// Openweather Location ID and API Key for meteo data retreival
+$OPENWEATHER_LOCATIONID = "openweather location id here";
+$OPENWEATHER_APIKEY = "openweather api key here";
 //---------------------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------------------
