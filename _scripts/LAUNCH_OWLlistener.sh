ba33ea4b3728efadd0424517a20904d9cf58b126
Stefano <48553214+vailatis@users.noreply.github.com>
2019-04-11 12:09:41 +0200

    Delete LAUNCH_OWLlistener.sh

--- _scripts/LAUNCH_OWLlistener.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-# Loop indefinitely until system shuts-down
-until [  1 -gt 10 ]; do
-   /usr/bin/python /var/www/_scripts/OWL_Listener.py
-   echo "Restarting listener process..."
-done
-
